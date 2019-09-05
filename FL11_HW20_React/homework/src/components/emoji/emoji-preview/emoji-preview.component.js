f0c5a12e42fab8810a0e0558b737b9a93f270dbd
Konstantin <sadovskyic@gmail.com>
2019-09-05 17:29:07 +0300

    Workshops: React

--- FL11_HW20_React/homework/src/components/emoji/emoji-preview/emoji-preview.component.js
+++ FL11_HW20_React/homework/src/components/emoji/emoji-preview/emoji-preview.component.js
@@ -1,6 +1,29 @@
 import React from 'react';
 import classes from './emoji-preview.module.scss';
 import PropTypes from 'prop-types';
+import StarIcon from '@material-ui/icons/Star';
+import StarHalfIcon from '@material-ui/icons/StarHalf';
+
+function rate(n) {
+  let stars = [];
+  for (let i = 1; i <= n; i++) {
+    stars.push(
+      React.createElement(StarIcon, {
+        className: classes.star,
+        fontSize: 'small'
+      })
+    );
+  }
+  if ((n ^ 0) !== n) {
+    stars.push(
+      React.createElement(StarHalfIcon, {
+        className: classes.star,
+        fontSize: 'small'
+      })
+    );
+  }
+  return stars;
+}
 
 export function EmojiPreview(props) {
   return (
@@ -10,9 +33,11 @@ export function EmojiPreview(props) {
         <div>{props.emoji2}</div>
         <div>{props.emoji3}</div>
       </div>
-      <div>{props.title}</div>
-      <div>{props.rate}</div>
-      <div>{props.price}</div>
+      <div className={classes.title}>{props.title}</div>
+      <div>
+        {rate(props.rate)}
+      </div>
+      <button className={classes.buy}>Get ({props.price}$)</button>
     </div>
   );
 }
